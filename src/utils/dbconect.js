cd6e08115269db92f54ac126cfb70b5c4ef5d7c8
Julian <julianrodriguez27@aragon.unam.mx>
2025-09-11 00:08:02 -0600

    Se elimino la dependencia de mongoose, dejo pendiente una restructuracion de archivos

---
+++ src/utils/dbconect.js
@@ -0,0 +1,12 @@
+import { MongoClient } from "mongodb";
+/*
+const MONGODB_URI = "";
+const MONGODB_DB = "";
+
+if (!MONGODB_URI) {
+    console.log("Error de conexion");
+    return;
+}
+
+const cached_client = null;
+*/
