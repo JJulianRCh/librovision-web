9d4e086b36b747f7f20769220f80acc5a42898cd
Julian <julianrodriguez27@aragon.unam.mx>
2025-09-11 16:57:20 -0600

    Se realizo la conexion a mongodb Cloud y se elimino la carptea <models> con sus respectivos archivos al ya no ser necesarios para el proyecto

--- src/utils/dbconect.js
+++ src/utils/dbconect.js
@@ -1,12 +1,22 @@
 import { MongoClient } from "mongodb";
-/*
-const MONGODB_URI = "";
-const MONGODB_DB = "";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const options = {};
 
 if (!MONGODB_URI) {
     console.log("Error de conexion");
     return;
 }
 
-const cached_client = null;
-*/
+let client;
+let clientPromise;
+
+if (!global._mongoClientPromise) {
+    client = new MongoClient(MONGODB_URI, options);
+    console.log("Conectado");
+    global._mongoClientPromise = client.connect();
+}
+
+clientPromise = global._mongoClientPromise;
+
+export default clientPromise;
