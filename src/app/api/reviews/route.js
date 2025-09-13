7731af66fe4e102735a326c18f9ed214a299c952
Julian <julianrodriguez27@aragon.unam.mx>
2025-09-13 09:51:41 -0600

    Se arreglo la parte del POST y GET de las reviews pero aun queda pendiente hacer que se visualicen

---
+++ src/app/api/reviews/route.js
@@ -0,0 +1,58 @@
+import { NextResponse } from "next/server";
+import clientPromise from "@/utils/dbconect";
+
+//POST-publicar resena
+export async function POST(req) {
+    try {
+        const {title, author, review, rank, userId} = req.json();
+
+        const client = (await clientPromise).db();
+        const bookreviews = client.collection("bookreviews")
+
+        await bookreviews.insertOne({
+            title: title,
+            author: author,
+            review: review,
+            rank: rank,
+            userId: userId
+        });
+        return NextResponse.json({
+            message: "Review Publicada",
+            status: 200
+        });
+    } catch (error) {
+        return NextResponse.json({});
+    }
+}
+
+//GET-enlistar resenas
+export async function GET(req) {
+    try {
+        const { searchParams } = new URL(req.url);
+        const title = searchParams.get("title");
+        const userId = searchParams.get("userId");
+        
+        const client = (await clientPromise).db();
+        const reviews = client.collection("bookreviews");
+
+        let query = {};
+        if (title) {
+            query.title = { $regex: new RegExp(title, "i") };
+        }
+        if (userId) {
+            query.userId = userId;
+        }
+
+        const result = await reviews.find(query).toArray();
+
+        return NextResponse.json({
+            reviews: reviews,
+            status: 200
+        });
+    } catch (error) {
+        return NextResponse.json({
+            message: error,
+            status: 500
+        });
+    }
+}
