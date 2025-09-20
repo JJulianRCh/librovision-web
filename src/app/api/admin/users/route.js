5d8bcd3f05f404b6ce3393e76af4350f22621d95
Julian <julianrodriguez27@aragon.unam.mx>
2025-09-20 02:07:07 -0600

    Panel Admin funcional

--- src/app/api/admin/users/route.js
+++ src/app/api/admin/users/route.js
@@ -5,7 +5,7 @@ export async function GET(req) {
     try {
         const client = (await clientPromise).db();
         const users = await client.collection("users").find().toArray();
-        return NextResponse.json(users);
+        return NextResponse.json({users: users});
     } catch(error) {
         return NextResponse.json({
             message: "Error al conseguir los usuarios",
